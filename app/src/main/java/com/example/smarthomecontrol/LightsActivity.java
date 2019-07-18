66f51a3c136ab8e2b942882ae5ae8cc34ef1c4da
Mohamed <Mohamedezzat2552@gmail.com>
2019-07-18 23:19:10 +0200

    initial commit

---
+++ app/src/main/java/com/example/smarthomecontrol/LightsActivity.java
@@ -0,0 +1,15 @@
+package com.example.smarthomecontrol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class LightsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lights);
+
+    }
+}
--- app/src/main/java/com/example/smarthomecontrol/MainActivity.java
+++ app/src/main/java/com/example/smarthomecontrol/MainActivity.java
@@ -1,25 +1,26 @@
 package com.example.smarthomecontrol;
 
-//import android.arch.lifecycle.ViewModelProvider;
+
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
-//import android.support.annotation.Nullable;
-//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
-
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
+
 
 public class MainActivity extends AppCompatActivity {
 
     MainViewModel viewModel;
     BluetoothAdapter bluetoothAdapter;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+
         if(!bluetoothAdapter.isEnabled()){
             Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivity(enableBT);
@@ -34,5 +35,21 @@ public class MainActivity extends AppCompatActivity {
 //
 //            }
 //        });
+
     }
+
+    public void temperatureControl(View view) {
+        intent = new Intent(getApplicationContext(), LightsActivity.class);
+        startActivity(intent);
+    }
+
+    public void setPool(View view) {
+    }
+
+    public void setCurtains(View view) {
+    }
+
+    public void setLights(View view) {
+    }
+
 }
